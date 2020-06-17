59a1f33586516f4683fa383faacf80b43d50e565
maguoliang <mglggch@gmail.com>
2020-06-17 10:22:02 +0800

    初次提交

---
+++ src/main/java/chinaPress/ChinaPressApplication.java
@@ -0,0 +1,13 @@
+package chinaPress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinaPressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChinaPressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chinaPress/ChinaPressApplicationTests.java
@@ -0,0 +1,13 @@
+package chinaPress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinaPressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
