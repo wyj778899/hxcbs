eddf338859ca0761014302448906ef143d250ccc
luchaoqun <531290028@qq.com>
2020-06-19 15:55:38 +0800

    启动成功文字

--- src/main/java/chinaPress/ChinaPressApplication.java
+++ src/main/java/chinaPress/ChinaPressApplication.java
@@ -8,7 +8,12 @@ public class ChinaPressApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChinaPressApplication.class, args);
-		System.out.println("启动成功");
+		System.out.println("    _____ _    _  _____ _____ ______  _____ _____ ");
+		System.out.println("   / ____| |  | |/ ____/ ____|  ____|/ ____/ ____|");
+		System.out.println("  | (___ | |  | | |   | |    | |__  | (___| (___  ");
+		System.out.println("   \\___ \\| |  | | |   | |    |  __|  \\___ \\\\___ \\ ");
+		System.out.println("   ____) | |__| | |___| |____| |____ ____) |___) |");
+		System.out.println("  |_____/ \\____/ \\_____\\_____|______|_____/_____/ ");
 	}
 
 }
