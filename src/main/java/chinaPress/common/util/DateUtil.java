751a54166f5c39ed66b2968e680df1670b1688f7
maguoliang <mglggch@gmail.com>
2020-06-19 15:26:05 +0800

    时间工具类

--- src/main/java/chinaPress/common/util/DateUtil.java
+++ src/main/java/chinaPress/common/util/DateUtil.java
@@ -1,19 +1,35 @@
 package chinaPress.common.util;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DateUtil {
-	
+
 	/**
 	 * 格式化日期
+	 * 
 	 * @author maguoliang
 	 * @param date
 	 * @param format
 	 * @return
 	 */
-	public String formatData(Date date, String format) {
+	public static String formatDate(Date date, String format) {
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
 		return simpleDateFormat.format(date);
+	}
+
+	/**
+	 * 日期转换
+	 * 
+	 * @author maguoliang
+	 * @param date
+	 * @param foramt
+	 * @return
+	 * @throws ParseException
+	 */
+	public static Date parseDate(String date, String foramt) throws ParseException {
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(foramt);
+		return simpleDateFormat.parse(date);
 	}
 }
