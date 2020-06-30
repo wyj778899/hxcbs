8f05c3c2f21cb6851d60b37df50a495cd6a3f6f0
张浩 <zhanghao@meilianma.com>
2020-06-30 22:37:46 +0800

    根据章节id查询关联课时

--- src/main/java/chinaPress/fc/course_section/service/FcCourseHourService.java
+++ src/main/java/chinaPress/fc/course_section/service/FcCourseHourService.java
@@ -1,6 +1,7 @@
 package chinaPress.fc.course_section.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -10,6 +11,7 @@ import chinaPress.common.util.ResultUtil;
 import chinaPress.fc.course_section.dao.FcCourseHourMapper;
 import chinaPress.fc.course_section.vo.FcCourseHourVo;
 import chinaPress.fc.order.service.FcOrderPersonService;
+import chinaPress.fc.order.service.FcOrderService;
 
 @Service
 public class FcCourseHourService {
@@ -18,6 +20,8 @@ public class FcCourseHourService {
 	private FcCourseHourMapper fcCourseHourMapper;
 	@Autowired
 	private FcOrderPersonService fcOrderPersonService;
+	@Autowired
+	private FcOrderService fcOrderService;
 	
 	/**
 	 * 根据章节id查询关联课时
@@ -27,14 +31,21 @@ public class FcCourseHourService {
 	public Result selectCourseHourListBySectionId(Integer personId,Integer courseId, Integer sectionId,Integer roleType){
 		List<FcCourseHourVo> data = fcCourseHourMapper.selectCourseHourListBySectionId(sectionId);
 		if(data.size()>0) {
-			int index = fcOrderPersonService.findPersonHourIsPass(personId, roleType, courseId, data.get(0).getId());
-			if(index == 1) {//通过
-				return ResultUtil.custom(1, "查询成功", data);
-			}else if(index == 0){
-				return ResultUtil.custom(0, "看过未考过", data);
+			
+			Map<String, Object> map = fcOrderService.findMyCourseIsExist(personId, roleType, courseId);
+			if(map.get("code").toString().equals("0")) {
+				int index = fcOrderPersonService.findPersonHourIsPass(personId, roleType, courseId, data.get(0).getId());
+				if(index == 1) {//通过
+					return ResultUtil.custom(1, "查询成功", data);
+				}else if(index == 0){
+					return ResultUtil.custom(0, "看过未考过", data);
+				}else {
+					return ResultUtil.custom(-1, "暂无视频");
+				}
 			}else {
-				return ResultUtil.custom(-1, "暂无视频");
+				return ResultUtil.custom(1, "免费试看", data);
 			}
+			
 		}else {
 			return ResultUtil.custom(-1, "暂无视频");
 		}
