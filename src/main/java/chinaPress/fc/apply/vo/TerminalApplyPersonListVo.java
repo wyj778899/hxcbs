626c700605139e8d0a3646a94e0e892b56fa1135
maguoliang <mglggch@gmail.com>
2020-07-09 20:17:06 +0800

    bug更改+代码格式化

--- src/main/java/chinaPress/fc/apply/vo/TerminalApplyPersonListVo.java
+++ src/main/java/chinaPress/fc/apply/vo/TerminalApplyPersonListVo.java
@@ -10,45 +10,59 @@ public class TerminalApplyPersonListVo {
 	private String name;
 
 	/**
-	 * 手机号
-	 */
-	private String tellPhone;
-
-	/**
 	 * 性别（1.男2.女）
 	 */
 	private Integer sex;
 
 	/**
+	 * 民族
+	 */
+	private String ethnic;
+
+	/**
+	 * 学历
+	 */
+	private String education;
+
+	/**
 	 * 证件号码
 	 */
 	private String certificateNumber;
-	
-	/**
-     * 年龄
-     */
-    private Integer age;
-    
-    /**
-     * 工作年限
-     */
-    private Integer workYear;
-    
-    /**
-     * 户籍地址
-     */
-    private String censusAddress;
-    
-    /**
-     * 岗位
-     */
-    private String post;
-    
-    /**
-     * 学历
-     */
-    private String education;
 
+	/**
+	 * 手机号
+	 */
+	private String tellPhone;
+
+	/**
+	 * 岗位
+	 */
+	private String post;
+
+	/**
+	 * 工作年限
+	 */
+	private Integer workYear;
+
+	/**
+	 * 籍贯
+	 */
+	private String nativePlace;
+
+	/**
+	 * 户籍地址
+	 */
+	private String censusAddress;
+
+	/**
+	 * 工作所在详细地址
+	 */
+	private String institutionAddress;
+
+	/**
+	 * 邮寄地址
+	 */
+	private String mailingAddress;
 
 	public Integer getId() {
 		return id;
@@ -90,14 +104,6 @@ public class TerminalApplyPersonListVo {
 		this.certificateNumber = certificateNumber;
 	}
 
-	public Integer getAge() {
-		return age;
-	}
-
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
 	public Integer getWorkYear() {
 		return workYear;
 	}
@@ -128,5 +134,61 @@ public class TerminalApplyPersonListVo {
 
 	public void setEducation(String education) {
 		this.education = education;
+	}
+
+	/**
+	 * 获取：nativePlace
+	 */
+	public String getNativePlace() {
+		return nativePlace;
+	}
+
+	/**
+	 * 设置：nativePlace
+	 */
+	public void setNativePlace(String nativePlace) {
+		this.nativePlace = nativePlace;
+	}
+
+	/**
+	 * 获取：institutionAddress
+	 */
+	public String getInstitutionAddress() {
+		return institutionAddress;
+	}
+
+	/**
+	 * 设置：institutionAddress
+	 */
+	public void setInstitutionAddress(String institutionAddress) {
+		this.institutionAddress = institutionAddress;
+	}
+
+	/**
+	 * 获取：mailingAddress
+	 */
+	public String getMailingAddress() {
+		return mailingAddress;
+	}
+
+	/**
+	 * 设置：mailingAddress
+	 */
+	public void setMailingAddress(String mailingAddress) {
+		this.mailingAddress = mailingAddress;
+	}
+
+	/**
+	 * 获取：ethnic
+	 */
+	public String getEthnic() {
+		return ethnic;
+	}
+
+	/**
+	 * 设置：ethnic
+	 */
+	public void setEthnic(String ethnic) {
+		this.ethnic = ethnic;
 	}
 }
