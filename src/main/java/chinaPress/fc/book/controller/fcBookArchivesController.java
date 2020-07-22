52ac5f19f715bcc08010c28dc391fae39c0e6c62
maguoliang <mglggch@gmail.com>
2020-07-22 18:30:11 +0800

    后台管理端书籍档案列表相关接口

--- src/main/java/chinaPress/fc/book/controller/fcBookArchivesController.java
+++ src/main/java/chinaPress/fc/book/controller/fcBookArchivesController.java
@@ -1,31 +1,34 @@
 package chinaPress.fc.book.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import chinaPress.common.result.model.Result;
+import chinaPress.common.util.ResultUtil;
 import chinaPress.fc.book.model.FcBookArchives;
 import chinaPress.fc.book.service.FcBookArchivesService;
-
+import chinaPress.fc.book.vo.FcBookArchivesManageListVo;
 
 /**
- * 20200717   终端书籍查询添加功能
+ * 20200717 终端书籍查询添加功能
+ * 
  * @author wyj
  *
  */
 @RestController
 @RequestMapping("/book")
-public class fcBookArchivesController {
+public class FcBookArchivesController {
 
-	
-	//书籍
+	// 书籍
 	@Autowired
 	FcBookArchivesService fcBookArchivesService;
-	
-	
+
 	/**
 	 * 添加书籍功能
+	 * 
 	 * @param fcBookArchives
 	 * @return
 	 */
@@ -33,10 +36,10 @@ public class fcBookArchivesController {
 	public Result registerBookArchives(FcBookArchives fcBookArchives) {
 		return fcBookArchivesService.addFcBookArchives(fcBookArchives);
 	}
-	
-	
+
 	/**
 	 * 更新书籍功能
+	 * 
 	 * @param fcBookArchives
 	 * @return
 	 */
@@ -44,15 +47,44 @@ public class fcBookArchivesController {
 	public Result modifyBookArchives(FcBookArchives fcBookArchives) {
 		return fcBookArchivesService.setFcBookArchives(fcBookArchives);
 	}
-	
-	
+
 	/**
 	 * 查询书籍功能
+	 * 
 	 * @param id
 	 * @return
 	 */
 	@RequestMapping("/queryBookArchives")
 	public Result queryBookArchives(Integer id) {
-		return fcBookArchivesService.findBooKInfos(id);
+		return fcBookArchivesService.findBookInfos(id);
+	}
+
+	/**
+	 * 查询书籍档案个数
+	 * 
+	 * @author maguoliang
+	 * @param name 书籍名称
+	 * @return
+	 */
+	@RequestMapping("manage/count")
+	public Result selectBookArchivesCount(String name) {
+		int count = fcBookArchivesService.selectBookArchivesCount(name);
+		return ResultUtil.ok(count);
+	}
+
+	/**
+	 * 查询书籍档案列表
+	 * 
+	 * @author maguoliang
+	 * @param name       书籍名称
+	 * @param pageNumber 第几页
+	 * @param pageSize   每页查询多少条数据
+	 * @return
+	 */
+	@RequestMapping("manage/list")
+	public Result selectBookArchivesList(String name, Integer pageNumber, Integer pageSize) {
+		List<FcBookArchivesManageListVo> list = fcBookArchivesService.selectBookArchivesList(name, pageNumber,
+				pageSize);
+		return ResultUtil.ok(list);
 	}
 }
--- src/main/java/chinaPress/fc/book/dao/FcBookArchivesMapper.java
+++ src/main/java/chinaPress/fc/book/dao/FcBookArchivesMapper.java
@@ -7,26 +7,49 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import chinaPress.fc.book.model.FcBookArchives;
+import chinaPress.fc.book.vo.FcBookArchivesManageListVo;
 
 @Mapper
 @Repository
 public interface FcBookArchivesMapper {
-    int deleteByPrimaryKey(Integer id);
+	int deleteByPrimaryKey(Integer id);
 
-    int insert(FcBookArchives record);
+	int insert(FcBookArchives record);
 
-    int insertSelective(FcBookArchives record);
+	int insertSelective(FcBookArchives record);
 
-    FcBookArchives selectByPrimaryKey(Integer id);
+	FcBookArchives selectByPrimaryKey(Integer id);
 
-    int updateByPrimaryKeySelective(FcBookArchives record);
+	int updateByPrimaryKeySelective(FcBookArchives record);
 
-    int updateByPrimaryKey(FcBookArchives record);
-    
-    /**
-     * id查询书籍信息，书籍信息包括书籍的内容和书籍的目录
-     * @param id
-     * @return
-     */
-    List<FcBookArchives> selectBookInfos(@Param("id")Integer id);
+	int updateByPrimaryKey(FcBookArchives record);
+
+	/**
+	 * id查询书籍信息，书籍信息包括书籍的内容和书籍的目录
+	 * 
+	 * @param id
+	 * @return
+	 */
+	List<FcBookArchives> selectBookInfos(@Param("id") Integer id);
+
+	/**
+	 * 查询书籍档案个数
+	 * 
+	 * @author maguoliang
+	 * @param name 书籍名称
+	 * @return
+	 */
+	int selectBookArchivesCount(@Param("name") String name);
+
+	/**
+	 * 查询书籍档案列表
+	 * 
+	 * @author maguoliang
+	 * @param name   书籍名称
+	 * @param offset 从哪一条数据开始查询
+	 * @param rows   查询多少条数据
+	 * @return
+	 */
+	List<FcBookArchivesManageListVo> selectBookArchivesList(@Param("name") String name,
+			@Param("offset") Integer offset, @Param("rows") Integer rows);
 }
