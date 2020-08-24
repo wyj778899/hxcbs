e150d465a4dce6d28215ff1b5c34495fa16a0d5c
maguoliang <mglggch@gmail.com>
2020-08-24 09:03:51 +0800

    报名申请新建页面课程列表对象

---
+++ src/main/java/chinaPress/fc/course/vo/FcExamSignupCourseListVo.java
@@ -0,0 +1,40 @@
+package chinaPress.fc.course.vo;
+
+public class FcExamSignupCourseListVo {
+	/**
+	 * 课程id
+	 */
+	private Integer id;
+	/**
+	 * 课程名称
+	 */
+	private String name;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
