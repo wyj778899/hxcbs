903f6475ea4f23112fad2ac7974a0fa3fc059e03
wangyajiang <yifenermaowu000@163.com>
2020-09-02 10:20:00 +0800

    证书返回实体类添加

---
+++ src/main/java/chinaPress/exam/exam_certificate/vo/ExamCertificate.java
@@ -0,0 +1,35 @@
+package chinaPress.exam.exam_certificate.vo;
+
+/**
+ * 考试证书信息数据回显
+ * @author wyj
+ *
+ */
+public class ExamCertificate {
+
+	private Integer id;
+	
+	
+	private String name;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
